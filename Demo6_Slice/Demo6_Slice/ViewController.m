defdd1c5086d4de9529c70ae51c5085ffcf5640e
susu <28025920@qq.com>
2016-04-02 12:29:43 +0800

    Slice-9切片技术的练习
    Slice-9切片技术的练习

---
+++ Demo6_Slice/Demo6_Slice/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  Demo6_Slice
+//
+//  Created by  江苏 on 16/2/24.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *seperatorImageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    UIImage* image=[UIImage imageNamed:@"delete_btn"];
+//    UIImage* resizedImage=[image resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10) resizingMode:UIImageResizingModeStretch];
+//    self.imageView.image=resizedImage;
+//    UIImage* image2=[UIImage imageNamed:@"seperator"];
+//    UIImage* resizedImage2=[image2 resizableImageWithCapInsets:UIEdgeInsetsMake(3, 4, 3, 4) resizingMode:UIImageResizingModeTile];
+//    self.seperatorImageView.image=resizedImage2;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
